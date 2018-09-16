d92c1cf6d1c2df1f5bd070b2c8d8cb75e02cef99
SirLich <lrkoehler77@gmail.com>
2018-09-16 13:00:52 -0500

    Utilities have been re-written to take a file instead of a line.

--- utilities.py
+++ utilities.py
@@ -5,21 +5,26 @@ import json
 
 def main():
     OUTPUT_LOCATION = "/home/liam/application_data/atom/reddit/VexillologyBot/locations.json"
-    location_name = sys.argv[1]
-    location_dict = create_dict(location_name)
+    INPUT_LOCATION = sys.argv[1]
 
-    with open(OUTPUT_LOCATION, "r+") as outfile:
-        data = json.load(outfile)
-        print(data)
-        data.append(location_dict)
-        print(data)
-    with open(OUTPUT_LOCATION, "wt") as outfile:
-        json.dump(data,outfile, indent=3)
+    with open(INPUT_LOCATION) as f:
+        for line in f:
+            location_name = line.split(":")[0].strip().replace("'","")
+            location_code = line.split(":")[1].strip().replace("'","").lower()
+            print(location_name + location_code)
+            location_dict = create_dict(location_name, location_code)
+            with open(OUTPUT_LOCATION, "r+") as outfile:
+                data = json.load(outfile)
+                print(data)
+                data.append(location_dict)
+                print(data)
+            with open(OUTPUT_LOCATION, "wt") as outfile:
+                json.dump(data,outfile, indent=3)
 
-def create_dict(location):
+def create_dict(location, location_code):
         d = {}
         d["display-name"] = location
-        d["svg-link"] = "null"
+        d["country-code"] = location_code;
         aliases = [location.lower()]
         d["aliases"] = aliases
         return d
